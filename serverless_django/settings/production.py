3e90c4663a3ced3717198aea94a781660805c05e
Chris Wolff <60451045+oriole04@users.noreply.github.com>
2021-05-21 15:41:00 -0500

    removed SECRET_KEY from settings files
    using:from decouple import config

--- serverless_django/settings/production.py
+++ serverless_django/settings/production.py
@@ -6,9 +6,14 @@ ALLOWED_HOSTS= ['windows-s3mqcan','www.ihm-chapel.com']
 DEBUG=False
 
 # SECURITY WARNING: keep the secret key used in production secret!
+<<<<<<< Updated upstream
 SECRET_KEY = os.environ.get("SECRET_KEY", ')w4^6y$l=c3fk9ag!&t5=__xw1+%+j00c)lfw8gmq+fg3&%2#h')
 
+=======
+SECRET_KEY = config('SECRET_KEY')
+>>>>>>> Stashed changes
 print ("Using Production")
+
 # Database 
 HOME_PAGE_MSG = "Welcome to the Immaculate Heart of Mary Catholic Church, SSPX: "
 BASIC_INFO_MSG = "Sunday Mass in Latin is at 6:30(Low Mass) and 9:00(High Mass) at the Acadamy, 777 221st Ave. Oak Grove; and Low Mass at 12:30 at the Chapel, 875 Manomen Ave. St. Paul"
