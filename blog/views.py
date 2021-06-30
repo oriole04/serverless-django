1d1bc442ef788fc59de81317645abb0b246ac8ed
Chris Wolff <60451045+oriole04@users.noreply.github.com>
2021-06-30 17:36:58 -0500

    Changed blog lookup from numbers to slug value
    ihm-chapel/blog/06-30-21 works

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
+# from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import BlogPost
 
-def blog_post_detail_page(request,post_id):
-    obj = get_object_or_404(BlogPost, id=post_id)      # query -> database
+def blog_post_detail_page(request,slug):
+    obj = get_object_or_404(BlogPost, slug=slug)      # query -> database
     template_name = 'blog_post_detail.html'
     context = {"object": obj}
     return render(request, template_name, context)
