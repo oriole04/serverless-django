237b0927f2105feeeb30d427dfecaa7ca22ac225
Chris Wolff <60451045+oriole04@users.noreply.github.com>
2021-06-30 16:51:02 -0500

    Better error handling
    get_object_or_404 added to BlogPost ID

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from .models import BlogPost
 
 def blog_post_detail_page(request,post_id):
-    obj = BlogPost.objects.get(id=post_id)    # query -> database
+    obj = get_object_or_404(BlogPost, id=post_id)      # query -> database
     template_name = 'blog_post_detail.html'
     context = {"object": obj}
     return render(request, template_name, context)
